1d2533206a324f8a2accd4e93fc5febd202d9bd3
Andrés Felipe Cortés Zambrano <133858271+Facz21@users.noreply.github.com>
2026-03-05 11:18:39 -0500

    Show the daily info of resources

---
+++ daily_info.py
@@ -0,0 +1,39 @@
+#Daily info
+            print("="*60)
+            print(f"Day: {day}")
+            print(f"Actual resources: wheat {wheat} - firewood {firewood} - gold {gold}  ")
+            print("="*60)
+            print(f"Daily event: {event}")
+            print(" ")
+            print(f"additional wheat loss {wheat_loss}")
+            print(" ")
+            print(f"additional firewood loss {firewood_loss}")
+            print(" ")
+            print(f"additional gold loss {gold_loss}")
+            print("="*60)
+            if wheat <= 0 or firewood <= 0 or gold <= 0:
+                    print("Your kingdom has fallen")
+            
+            else:
+                print("="*60)
+                keep_playing = input("move on the next day? (Y/N): ").upper()
+                
+
+            print(f"Day: {day}")
+            print(f"Actual resources: wheat {wheat} - firewood {firewood} - gold {gold}  ")
+            print("="*60)
+            print(f"Daily event: {event}")
+            print(" ")
+            print(f"additional wheat loss {wheat_loss}")
+            print(" ")
+            print(f"additional firewood loss {firewood_loss}")
+            print(" ")
+            print(f"additional gold loss {gold_loss}")
+            print("="*60)
+            if wheat <= 0 or firewood <= 0 or gold <= 0:
+                    print("Your kingdom has fallen")
+            
+            else:
+                print("="*60)
+                keep_playing = input("move on the next day? (Y/N): ").upper()
+                
